734c6931f287aeaa1c618db66ed233752543c5fc
rohans1445 <rohans1445@gmail.com>
2022-10-12 22:24:37 +0530

    time validations for cancellations

--- src/main/java/com/example/transportapi/util/AppConstants.java
+++ src/main/java/com/example/transportapi/util/AppConstants.java
@@ -1,7 +1,10 @@
 package com.example.transportapi.util;
 
 public class AppConstants {
-    public static Integer PER_TRIP_COST = 120;
-    public static Integer PER_TRIP_COST_MONTHLY = 90;
-    public static Integer MINIMUM_DAYS_REQUIRED_FOR_HYBRID_PASS = 4;
+    public static final Integer PER_TRIP_COST = 120;
+    public static final Integer PER_TRIP_COST_MONTHLY = 90;
+    public static final Integer MINIMUM_DAYS_REQUIRED_FOR_HYBRID_PASS = 4;
+    public static final Integer CUTOFF_TIME_FOR_PASS_BOOKING_IN_HOURS = 6;
+    public static final Integer CUTOFF_TIME_FOR_PASS_CANCELLATION_IN_HOURS = 6;
+    public static final Integer CUTOFF_TIME_FOR_TRIP_CANCELLATION_IN_HOURS = 6;
 }
